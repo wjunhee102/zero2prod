be815512cac11848917792d768608c7d3fd91691
junhee <59816563+wjunhee102@users.noreply.github.com>
2026-07-22 10:33:54 +0900

    [fix] 구문 에러 수정

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use actix_web::{web, App, HttpRequest, HttpServer, Responder};
 
-async fn greet(req: HttpRequst) -> impl Responder {
+async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
     format!("Hello, {}!", name)
 }
